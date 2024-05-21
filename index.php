c17a9de76ee5c9fa46f5b80097fd19d732c895e5
Robertarezza <roberta.rezza@gmail.com>
2024-05-21 14:49:45 +0200

    add: table for bootstrap

--- index.php
+++ index.php
@@ -49,11 +49,21 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <!-- Style -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- bootstrap -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<title>PHP Hotel </title>
 </head>
 
 <body>
-    <?php foreach ($hotels as $cur_hotels) { ?>
+
+<!-- stampa senza bootstrap -->
+    <!-- <?php foreach ($hotels as $cur_hotels) { ?>
         <div>
             <h4><?php echo $cur_hotels["name"]; ?></h4>
             <p>Descrizione: <?php echo $cur_hotels["description"]; ?></p>
@@ -67,7 +77,58 @@ $hotels = [
 
         <p>Distanza dal centro: <?php echo $cur_hotels["distance_to_center"]; ?> km</p>
         </div>
-    <?php } ?>
+    <?php } ?> -->
+    <!-- /stampa senza bootstrap -->
+
+    <!-- stampa con bootstrap -->
+
+    <div class="title">
+        <h1>Lista di Hotel</h1>
+    </div>
+    <table class="table table-bordered border-primary">
+        <thead>
+
+        <!-- titolo -->
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Distanza dal centro</th>
+                <th scope="col">Voto</th>
+            </tr>
+            <!-- /titolo -->
+        </thead>
+
+        <!-- corpo -->
+        <tbody>
+            <?php foreach ($hotels as $cur_hotels) { ?>
+                <tr>
+                    <th scope="row"><?php echo $cur_hotels["name"]; ?></th>
+                    <td><?php echo $cur_hotels["description"]; ?></td>
+                    <td>
+                        <?php
+                        if ($cur_hotels["parking"] === true) {
+                            echo "presente";
+                        } else {
+                            echo "non presente";
+                        }
+                        ?>
+                    </td>
+                    <td><?php echo $cur_hotels["distance_to_center"]; ?> km</td>
+                    <td><?php echo $cur_hotels["vote"]; ?></td>
+                </tr>
+            <?php } ?>
+               <!-- /corpo -->
+        </tbody>
+    </table>
+
+<!-- /stampa con bootstrap -->
+
+
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
